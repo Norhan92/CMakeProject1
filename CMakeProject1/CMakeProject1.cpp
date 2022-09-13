81879fcfadc21f3ecfda17a40f08c30fe0bcdde1
mohsen <mohsen@W106DSZHG3>
2022-09-13 20:52:00 +0200

    Add project files.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,15 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+//#include "CMakeProject1.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "test";
+	return 0;
+}
